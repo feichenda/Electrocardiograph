43b0a7a89f7e15a35813a57230ba1c1ad4697490
肥仔一只 <c1376479276@163.com>
2021-05-13 09:18:22 +0800

    version 1.0

--- app/src/main/java/com/lenovo/feizai/electrocardiograph/activity/LoginActivity.java
+++ app/src/main/java/com/lenovo/feizai/electrocardiograph/activity/LoginActivity.java
@@ -1,7 +1,17 @@
 package com.lenovo.feizai.electrocardiograph.activity;
 
+import android.view.WindowManager;
+import android.widget.TextView;
+
 import com.lenovo.feizai.electrocardiograph.R;
 import com.lenovo.feizai.electrocardiograph.base.BaseActivity;
+
+import java.util.Date;
+import java.util.Timer;
+import java.util.TimerTask;
+
+import butterknife.BindView;
+import butterknife.OnClick;
 
 /**
  * @author feizai
@@ -9,12 +19,56 @@ import com.lenovo.feizai.electrocardiograph.base.BaseActivity;
  */
 public class LoginActivity extends BaseActivity {
 
+    @BindView(R.id.time)
+    TextView time;
+
+    private Timer timer;
+    private int count;
+
     public LoginActivity() {
         super(R.layout.activity_login);
     }
 
     @Override
     protected void initView() {
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);//隐藏状态栏
+        count = 5;
+        timer = new Timer();
+        TimerTask task1 = new TimerTask() {
+            @Override
+            public void run() {
+                if (count > 0) {
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            time.setText(count+"S");
+                            count--;
+                        }
+                    });
+                }
+                if (count == 0) {
+                    timer.cancel();
+                    startActivity(TestActivity.class);
+                    finish();
+                }
+            }
+        };
+        timer.schedule(task1,2000,1000);
+    }
 
+    @OnClick(R.id.enter)
+    public void enter() {
+        count = 0;
+        timer.cancel();
+        startActivity(TestActivity.class);
+        finish();
+    }
+
+    @OnClick(R.id.jump)
+    public void jump() {
+        count = 0;
+        timer.cancel();
+        startActivity(TestActivity.class);
+        finish();
     }
 }
